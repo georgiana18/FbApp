3966eee98abba0ad59c4026ed60fb185cdc5d355
Georgiana Asavoaei <georgiana.asavoaei@gmail.com>
2019-12-08 23:45:34 +0200

    Add project files.

---
+++ FbApp/Models/IdentityModels.cs
@@ -0,0 +1,50 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Security.Claims;
+using System.Threading.Tasks;
+using System.Web.Mvc;
+using Microsoft.AspNet.Identity;
+using Microsoft.AspNet.Identity.EntityFramework;
+
+namespace FbApp.Models
+{
+    public class ApplicationUser : IdentityUser
+    {
+        public string Gender { get; set; }
+
+        public DateTime DateOfBirth { get; set; }
+
+        public string Country { get; set; }
+
+        // public Photo ProfilePhoto { get; set; }
+
+        //public ICollection<Album> Albums { get; set; }
+
+        public ApplicationUser() : base()
+        {
+            DateOfBirth = DateTime.Now;
+        }
+
+        public IEnumerable<SelectListItem> AllRoles { get; set; }
+
+        public async Task<ClaimsIdentity> GenerateUserIdentityAsync(UserManager<ApplicationUser> manager)
+        {
+            var userIdentity = await manager.CreateIdentityAsync(this, DefaultAuthenticationTypes.ApplicationCookie);
+
+            return userIdentity;
+        }
+    }
+
+    public class ApplicationDbContext : IdentityDbContext<ApplicationUser>
+    {
+        public ApplicationDbContext()
+            : base("DefaultConnection", throwIfV1Schema: false)
+        {
+        }
+
+        public static ApplicationDbContext Create()
+        {
+            return new ApplicationDbContext();
+        }
+    }
+}
